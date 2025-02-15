2ff35733634b57e9febc55adb7ee0c9030440749
Elkomey <elkomey2004@gmail.com>
2025-02-16 00:14:02 +0200

    Adding Filtering , Sorting and Pagination

--- NZWalks.API/Repositories/IWalkRepository.cs
+++ NZWalks.API/Repositories/IWalkRepository.cs
@@ -1,10 +1,11 @@
-﻿  using NZWalks.API.Models.Domain;
+﻿using NZWalks.API.Models.Domain;
 
 namespace NZWalks.API.Repositories
 {
 	public interface IWalkRepository 
 	{
-		Task<List<Walk>> GetAllAsync();
+		Task<List<Walk>> GetAllAsync(string? filterOn = null , string? filterQuery = null ,
+			string? sortBy = null ,  bool isAscending = true , int pageNumber = 1 , int pageSize = 1000);
 
 		Task<Walk?> GetByIdAsync(Guid id);
 
